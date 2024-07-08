91ce83207e13013052af4fa0e20126dbd4b14f0d
karthik18495 <karthik18495@gmail.com>
2024-07-08 13:23:15 -0400

    chore: Update configuration template and training script

--- Day-1/HPO-SLURM-hands_on/scripts/train.py
+++ Day-1/HPO-SLURM-hands_on/scripts/train.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.neural_network import MLPClassifier
 import json
+from joblib import dump
 
 def runTraining(args):
     # Load the configuration from a JSON file
@@ -30,7 +31,9 @@ def runTraining(args):
                         solver=config['solver'],
                         alpha=config['alpha'],
                         max_iter=config['max_iter'],
-                        random_state=config['random_state'])
+                        random_state=config['random_state'],
+                        verbose=True
+                        )
 
     # Train the model
     model.fit(X_train, y_train)
@@ -40,39 +43,14 @@ def runTraining(args):
     print(f"Accuracy: {accuracy}")
 
     # Save the trained model
-    model.save('trained_model.pkl')
+    dump(model, 'trained_model.pkl')
 
-    # Download the MNIST digit data
-    X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
-
-    # Split the data into training and testing sets
-    n_samples = len(X)
-    n_train = int(n_samples * config['train_ratio'])
-    X_train, y_train = X[:n_train], y[:n_train]
-    X_test, y_test = X[n_train:], y[n_train:]
-
-    # Create a neural network model
-    model = MLPClassifier(hidden_layer_sizes=config['hidden_layer_sizes'],
-                        activation=config['activation'],
-                        solver=config['solver'],
-                        alpha=config['alpha'],
-                        max_iter=config['max_iter'],
-                        random_state=config['random_state'])
-
-    # Train the model
-    model.fit(X_train, y_train)
-
-    # Evaluate the model
-    accuracy = model.score(X_test, y_test)
-    print(f"Accuracy: {accuracy}")
     with open("results.json", "w") as f:
         json.dump({"accuracy": accuracy}, f)
-
-    # Save the trained model
-    model.save('trained_model.pkl')
-
-
-# Parse command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, help='Path to the configuration file')
-args = parser.parse_args()
+if __name__ == '__main__':
+    
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, help='Path to the configuration file')
+    args = parser.parse_args()
+    runTraining(args)
